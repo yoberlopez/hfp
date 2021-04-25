95f10ffc59d3e54d7f3fa4ed70d2e36098005250
peppo-su <ramanujanmanwho@gmail.com>
2021-04-25 18:54:59 -0500

    d-add a loop, game v1.0

--- guessing-game.py
+++ guessing-game.py
@@ -1,11 +1,13 @@
 print("Welcome!")
-g = input("Guess the number: ")
-guess = int(g)
-if guess == 5:
-    print("You win!")
-else:
-    if guess > 5:
-        print("Too high")
+guess = 0
+while guess != 5:
+    g = input("Guess the number: ")
+    guess = int(g)
+    if guess == 5:
+        print("You win!")
     else:
-        print("Too low")
+        if guess > 5:
+            print("Too high")
+        else:
+            print("Too low")
 print("Game over!")
